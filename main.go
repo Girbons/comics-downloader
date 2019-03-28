ba3d89a3922980c979e122e0fa9620159a3bd8c3
Alessandro De Angelis <alessandrodea22@gmail.com>
2019-03-28 21:18:44 +0100

    Support multiple urls

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/Girbons/comics-downloader/pkg/detector"
 	"github.com/Girbons/comics-downloader/pkg/loader"
@@ -14,7 +15,7 @@ func main() {
 	// use log INFO Level
 	log.SetLevel(log.InfoLevel)
 	// arguments setup
-	url := flag.String("url", "", "Comic URL")
+	url := flag.String("url", "", "Comic URL or Comic URLS by separating each site with a comma without the use of spaces")
 	format := flag.String("format", "pdf", "Comic format output, supported formats are pdf,epub,cbr,cbz")
 	country := flag.String("country", "", "Set the country to retrieve a manga, Used by MangaRock")
 	flag.Parse()
@@ -25,25 +26,31 @@ func main() {
 		os.Exit(1)
 	}
 
-	// check if the url is supported
-	source, check := detector.DetectComic(*url)
-	if !check {
-		log.WithFields(log.Fields{
-			"site": *url,
-		}).Error("This site is not supported :(")
-		os.Exit(1)
+	if !strings.HasSuffix(*url, ",") {
+		*url = *url + ","
 	}
 
 	// check if the format is supported
 	if !detector.DetectFormatOutput(*format) {
 		log.WithFields(log.Fields{
 			"format": *format,
-		}).Info("Format not supported pdf will be used instead")
+		}).Error("Format not supported pdf will be used instead")
 	}
 
-	// in case the url is supported
-	// setup the right strategy to parse a comic
-	comic := loader.LoadComicFromSource(source, *url, *country)
-	comic.SetFormat(*format)
-	comic.MakeComic()
+	for _, u := range strings.Split(*url, ",") {
+		if u != "" {
+			// check if the url is supported
+			source, check := detector.DetectComic(u)
+			if !check {
+				log.WithFields(log.Fields{"site": u}).Error("This site is not supported :(")
+				continue
+			}
+			log.WithFields(log.Fields{"url": u}).Info("Downloading...")
+			// in case the url is supported
+			// setup the right strategy to parse a comic
+			comic := loader.LoadComicFromSource(source, u, *country)
+			comic.SetFormat(*format)
+			comic.MakeComic()
+		}
+	}
 }
