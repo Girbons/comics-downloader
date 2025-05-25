64d4498e23880d7483ba32bc5b042a5a196ee62f
Pontoporeia <pontoporeia@happyngreen.fr>
2025-05-25 14:56:50 +0200

    Fix: Using os.Getwd() instead of args or CurrentDir() function. Fixes https://github.com/Girbons/comics-downloader/issues/165.

--- pkg/util/path.go
+++ pkg/util/path.go
@@ -9,13 +9,11 @@ import (
 // createPath create folders given the path.
 func createPath(path string) (string, error) {
 	err := os.MkdirAll(path, os.ModePerm)
-
 	if err != nil {
 		return path, err
 	}
 
 	dir, err := filepath.Abs(path)
-
 	if err != nil {
 		return dir, err
 	}
@@ -49,9 +47,13 @@ func ImagesPathSetup(createDefaultPath bool, outputFolder, source, name, issueFo
 	return createPath(path)
 }
 
-// CurrentDir returns the path where the executable is
+// CurrentDir returns the path where the executable was called.
 func CurrentDir() (string, error) {
-	return filepath.Abs(filepath.Dir(os.Args[0]))
+	exePath, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	return exePath, nil
 }
 
 // DirectoryOrFileDoesNotExist check if a directory/file exist.
