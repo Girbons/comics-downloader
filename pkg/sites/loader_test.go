3fd623eaca87b69e4ee12638dec03c50dd5a2f5a
ProjectDislocate <173194962+ProjectDislocate@users.noreply.github.com>
2026-03-06 14:19:22 -0500

    fix: missing assert import

--- pkg/sites/loader_test.go
+++ pkg/sites/loader_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Girbons/comics-downloader/pkg/config"
 	"github.com/Girbons/comics-downloader/pkg/core"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
